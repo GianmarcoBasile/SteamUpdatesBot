014971e833bdfdbb0ae2dc8ebeaaf0df9c6389bc
GianmarcoBasile <basilemarc2000@gmail.com>
2023-11-06 18:56:29 +0100

    chore: add src and tests folder

---
+++ src/bot.py
@@ -0,0 +1,12 @@
+"""Module providing Bot API."""
+
+from telegram.ext import Application
+class Bot():
+    """Provides the bot API."""
+    def __init__(self, token):
+        """Bot Constructor."""
+        self.token = token
+        self.application = Application.builder().token(self.token).build()
+    def get_token(self):
+        """Return the API Token."""
+        return self.token
---
+++ src/database.py
@@ -0,0 +1,7 @@
+"""Module providing Database API."""
+
+import redis
+def initialize_db(host, port):
+    """Initialize Database."""
+    db = redis.Redis(host=host, port=port, decode_responses=True)
+    return db
