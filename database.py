579c771a466161fc159df1f51d42288ae5696f36
GianmarcoBasile <basilemarc2000@gmail.com>
2023-11-17 17:43:15 +0100

    refactor: code rebuild

--- database.py
+++
@@ -1,7 +0,0 @@
-"""Module providing Database API."""
-
-import redis
-def initialize_db(host, port):
-    """Initialize Database."""
-    db = redis.Redis(host=host, port=port, decode_responses=True)
-    return db
