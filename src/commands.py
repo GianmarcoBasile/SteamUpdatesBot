e23193bf39e8e6aedddb10fe6b21380a778027fb
Andrysora <andrea.seminara01@gmail.com>
2023-12-11 17:04:56 +0100

    feat:added deletegame command

--- src/commands.py
+++ src/commands.py
@@ -38,6 +38,30 @@ async def addGame(update, context):
             await update.message.reply_text('La sintassi del comando prevede un argomento: /addgame <game_id>')
     except Exception as e:
         print(e)
+
+async def deleteGame(update, context):    
+    if context.args:
+        game_name = ' '.join(context.args).lower()
+        games_list = json.loads(redis_instance.get(update.message.from_user['username']))['games']
+        if not games_list:
+            await update.message.reply_text('La lista dei giochi preferiti è vuota')
+        else:
+            games_json = json.loads(redis_instance.get(update.message.from_user['username']))
+            if game_name in games_json['games'].values():
+                for key, value in games_json['games'].items():
+                    if value == game_name:
+                        games_json['games'].pop(key)
+                        break                
+                new_games_json = {'games':{}}
+                for key, value in games_json['games'].items():
+                    new_games_json['games'].update({len(new_games_json['games']): value})
+                redis_instance.set(update.message.from_user['username'], json.dumps(new_games_json))
+                await update.message.reply_text('Gioco ' + str(game_name)+ ' eliminato dalla lista ')
+            else:
+                await update.message.reply_text('Il gioco non è nella lista, impossibile eliminarlo')
+    else:
+        await update.message.reply_text('La sintassi del comando richiede un argomento: /deletegame <game_name>')
+    
     
 async def clearGamesList(update, context):
     redis_instance.set(update.message.from_user['username'], json.dumps({'games': {}}))
