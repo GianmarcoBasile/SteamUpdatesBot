7f9af7a612151fe9f188c5451912017197c0f804
Andrysora <andrea.seminara01@gmail.com>
2023-12-12 13:13:55 +0100

    feat:mongodb compatibility

--- src/commands.py
+++ src/commands.py
@@ -5,6 +5,7 @@ import json
 import requests
 from database import initialize_db as db
 from utils import get_game_list, get_game_id_by_name
+from news_parser import parser
 
 mongo_instance = db('mongodb://localhost', 27017)
 app_list = get_game_list()
@@ -14,7 +15,7 @@ async def start(update, context):
     sender = update.message.from_user['username']
     if not mongo_instance['USERS'][sender].find_one({'chat_id': update.message.chat_id}):
         mongo_instance['USERS'][sender].insert_one({'chat_id': update.message.chat_id})
-        mongo_instance['GAMES']['games'].insert_one({'user': sender, 'games': {}})
+        mongo_instance['GAMES']['games'].insert_one({'user': sender, 'games': {}, 'last_news':{}})
     await update.message.reply_text('Welcome to Steam News Bot!')
 
 async def addGame(update, context):
@@ -52,20 +53,30 @@ async def deleteGame(update, context):
         else:
             await update.message.reply_text('The game is not in the list')
     else:
-        await update.message.reply_text(required_argument + '/deletegame <game_name>')
+        await update.message.reply_text(syntax_error + '/deletegame <game_name>')
     
     
 async def clearGamesList(update, context):
-    redis_instance.set(update.message.from_user['username'], json.dumps({'games': {}}))
-    await update.message.reply_text('Game list cleared')
+    if not context.args:
+        games_record = mongo_instance['GAMES']['games'].find_one({'user': update.message.from_user['username']})
+        games_record['games'] = {}
+        mongo_instance['GAMES']['games'].update_one({'user': update.message.from_user['username']}, {'$set': games_record})
+        await update.message.reply_text('Game list cleared')
+    else:
+        await update.message.reply_text(syntax_error + '/cleargameslist')
 
 async def getNews(update, context):
     if not context.args:
-        games = json.loads(redis_instance.get(update.message.from_user['username']))['games']
-        games_news_record = json.loads(redis_instance.get(update.message.from_user['username']))['games']
-        for game in games.values():
-            r = requests.get('http://api.steampowered.com/ISteamNews/GetNewsForApp/v0002/?appid=' + str(get_game_id_by_name(game)) + '&count=3&maxlength=300&format=json')
-            print(r.json())
+        games = mongo_instance['GAMES']['games'].find_one({'user': update.message.from_user['username']})
+        for game in games['games'].values():
+            r = requests.get('http://api.steampowered.com/ISteamNews/GetNewsForApp/v0002/?appid=' + str(get_game_id_by_name(game)) + '&count=10&maxlength=50000&format=json')
+            news_list = r.json()['appnews']['newsitems']
+            for news in news_list:
+                if games['last_news'] == {}:
+                    games['last_news'] = {'0': news['gid']}
+                elif news['gid'] not in games['last_news'].values():
+                    games['last_news'].update({str(int(max(games['last_news']))+1): news['gid']})   
+            mongo_instance['GAMES']['games'].update_one({'user': update.message.from_user['username']}, {'$set': games})
 
     else:
         await update.message.reply_text(syntax_error + '/getnews')
@@ -78,6 +89,9 @@ async def getNews(update, context):
 #     return r.json()
 
 async def getFavoriteGames(update, context):
-    games = list(json.loads(redis_instance.get(update.message.from_user['username']))['games'].values())
-    print(games)
-    await update.message.reply_text('Favorite Games: ' + str(games).replace('[', '').replace(']', '').replace("'", ''))
+    if not context.args:
+        games_record = mongo_instance['GAMES']['games'].find_one({'user': update.message.from_user['username']})
+        games = list(games_record['games'].values())
+        await update.message.reply_text('Favorite Games: ' + str(games).replace('[', '').replace(']', '').replace("'", ''))
+    else:
+        await update.message.reply_text(syntax_error + '/favoritegames')
