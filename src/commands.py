bcb113e0cf464b81499a513a4180fc43e74663e3
GianmarcoBasile <basilemarc2000@gmail.com>
2023-12-15 19:06:04 +0100

    chore: change news message

--- src/commands.py
+++ src/commands.py
@@ -8,74 +8,111 @@ from utils import get_game_list, get_game_id_by_name
 from news_parser import parser
 from telegram.constants import ParseMode
 
-mongo_instance = db('mongodb://localhost', 27017)
+mongo_instance = db("mongodb://localhost", 27017)
 app_list = get_game_list()
-syntax_error = 'The correct syntax for this command is: '
+syntax_error = "The correct syntax for this command is: "
+
 
 async def start(update, context):
-    sender = update.message.from_user['username']
-    if not mongo_instance['USERS'][sender].find_one({'chat_id': update.message.chat_id}):
-        mongo_instance['USERS'][sender].insert_one({'chat_id': update.message.chat_id})
-        mongo_instance['GAMES']['games'].insert_one({'user': sender, 'games': {}})
-    await update.message.reply_text('Welcome to Steam News Bot!')
+    sender = update.message.from_user["username"]
+    if not mongo_instance["USERS"][sender].find_one(
+        {"chat_id": update.message.chat_id}
+    ):
+        mongo_instance["USERS"][sender].insert_one({"chat_id": update.message.chat_id})
+        mongo_instance["GAMES"]["games"].insert_one({"user": sender, "games": {}})
+    await update.message.reply_text("Welcome to Steam News Bot!")
+
 
 async def addGame(update, context):
     try:
         if context.args:
-            game_name = ' '.join(context.args).lower()
+            game_name = " ".join(context.args).lower()
             if game_name in app_list.values():
-                games_record = mongo_instance['GAMES']['games'].find_one({'user': update.message.from_user['username']})
+                games_record = mongo_instance["GAMES"]["games"].find_one(
+                    {"user": update.message.from_user["username"]}
+                )
                 print(games_record)
-                if game_name not in games_record['games'].values():
-                    if(games_record['games'] == {}):
-                        games_record['games'] = {'0': game_name}
+                if game_name not in games_record["games"].values():
+                    if games_record["games"] == {}:
+                        games_record["games"] = {"0": game_name}
                     else:
-                        games_record['games'].update({str(int(max(games_record['games']))+1): game_name})
+                        games_record["games"].update(
+                            {str(int(max(games_record["games"])) + 1): game_name}
+                        )
                     print(games_record)
-                    mongo_instance['GAMES']['games'].update_one({'user': update.message.from_user['username']}, {'$set': games_record})
-                    await update.message.reply_text('Game ' + str(game_name) + ' added to the list')
+                    mongo_instance["GAMES"]["games"].update_one(
+                        {"user": update.message.from_user["username"]},
+                        {"$set": games_record},
+                    )
+                    await update.message.reply_text(
+                        "Game " + str(game_name) + " added to the list"
+                    )
                 else:
-                    await update.message.reply_text('The game is already in the list')
+                    await update.message.reply_text("The game is already in the list")
             else:
-                await update.message.reply_text('The game is not a steam game')
+                await update.message.reply_text("The game is not a steam game")
         else:
-            await update.message.reply_text(syntax_error + '/addgame <game_name>')
+            await update.message.reply_text(syntax_error + "/addgame <game_name>")
     except Exception as e:
         print(e)
 
-async def deleteGame(update, context):    
+
+async def deleteGame(update, context):
     if context.args:
-        game_name = ' '.join(context.args).lower()
-        games_record = mongo_instance['GAMES']['games'].find_one({'user': update.message.from_user['username']})
-        if game_name in games_record['games'].values():
-            games_record['games'].pop(str(list(games_record['games'].values()).index(game_name)))
-            mongo_instance['GAMES']['games'].update_one({'user': update.message.from_user['username']}, {'$set': games_record})
-            await update.message.reply_text('Game ' + str(game_name) + ' deleted from the list')
+        game_name = " ".join(context.args).lower()
+        games_record = mongo_instance["GAMES"]["games"].find_one(
+            {"user": update.message.from_user["username"]}
+        )
+        if game_name in games_record["games"].values():
+            games_record["games"].pop(
+                str(list(games_record["games"].values()).index(game_name))
+            )
+            mongo_instance["GAMES"]["games"].update_one(
+                {"user": update.message.from_user["username"]}, {"$set": games_record}
+            )
+            await update.message.reply_text(
+                "Game " + str(game_name) + " deleted from the list"
+            )
         else:
-            await update.message.reply_text('The game is not in the list')
+            await update.message.reply_text("The game is not in the list")
     else:
-        await update.message.reply_text(syntax_error + '/deletegame <game_name>')
-    
-    
+        await update.message.reply_text(syntax_error + "/deletegame <game_name>")
+
+
 async def clearGamesList(update, context):
     if not context.args:
-        games_record = mongo_instance['GAMES']['games'].find_one({'user': update.message.from_user['username']})
-        games_record['games'] = {}
-        mongo_instance['GAMES']['games'].update_one({'user': update.message.from_user['username']}, {'$set': games_record})
-        await update.message.reply_text('Game list cleared')
+        games_record = mongo_instance["GAMES"]["games"].find_one(
+            {"user": update.message.from_user["username"]}
+        )
+        games_record["games"] = {}
+        mongo_instance["GAMES"]["games"].update_one(
+            {"user": update.message.from_user["username"]}, {"$set": games_record}
+        )
+        await update.message.reply_text("Game list cleared")
     else:
-        await update.message.reply_text(syntax_error + '/cleargameslist')
+        await update.message.reply_text(syntax_error + "/cleargameslist")
+
 
 async def getNews(update, context):
     if not context.args:
-        games = mongo_instance['GAMES']['games'].find_one({'user': update.message.from_user['username']})
-        for game in games['games'].values():
-            r = requests.get('http://api.steampowered.com/ISteamNews/GetNewsForApp/v0002/?appid=' + str(get_game_id_by_name(game)) + '&count=3&maxlength=50000&format=json')
-            news_list = r.json()['appnews']['newsitems']
-            #added_news = []
+        games = mongo_instance["GAMES"]["games"].find_one(
+            {"user": update.message.from_user["username"]}
+        )
+        for game in games["games"].values():
+            print("AAAAAAAAA", get_game_id_by_name(game))
+            r = requests.get(
+                "http://api.steampowered.com/ISteamNews/GetNewsForApp/v0002/?appid="
+                + str(get_game_id_by_name(game))
+                + "&count=1&maxlength=50000&format=json"
+            )
+            print(r.json())
+            news_list = r.json()["appnews"]["newsitems"]
+            # added_news = []
             for news in news_list:
                 news_message = parser(news)
-                await update.message.reply_text(news_message, parse_mode=ParseMode.HTML) #make message returned prettier
+                await update.message.reply_text(
+                    news_message, parse_mode=ParseMode.HTML
+                )  # make message returned prettier
             #     if games['last_news'] == {}:
             #         games['last_news'] = {'0': news['gid']}
             #     elif news['gid'] not in games['last_news'].values():
@@ -89,7 +126,8 @@ async def getNews(update, context):
             #         #news_to_print = parser(news_to_parse)
 
     else:
-        await update.message.reply_text(syntax_error + '/getnews')
+        await update.message.reply_text(syntax_error + "/getnews")
+
 
 # async def getNews(context):
 #     global game_id
@@ -98,10 +136,16 @@ async def getNews(update, context):
 #     print(r.json())
 #     return r.json()
 
+
 async def getFavoriteGames(update, context):
     if not context.args:
-        games_record = mongo_instance['GAMES']['games'].find_one({'user': update.message.from_user['username']})
-        games = list(games_record['games'].values())
-        await update.message.reply_text('Favorite Games: ' + str(games).replace('[', '').replace(']', '').replace("'", ''))
+        games_record = mongo_instance["GAMES"]["games"].find_one(
+            {"user": update.message.from_user["username"]}
+        )
+        games = list(games_record["games"].values())
+        await update.message.reply_text(
+            "Favorite Games: "
+            + str(games).replace("[", "").replace("]", "").replace("'", "")
+        )
     else:
-        await update.message.reply_text(syntax_error + '/favoritegames')
+        await update.message.reply_text(syntax_error + "/favoritegames")
--- src/news_parser.py
+++ src/news_parser.py
@@ -1,47 +1,45 @@
 """Module providing parsing for the game news"""
 
+from datetime import datetime
 from utils import get_game_name_by_id
+
 
 def parser(news):
     news_to_return = {}
-    news_to_return.update({'game' : get_game_name_by_id(news['appid'])})
-    news_to_return.update({'title' : news['title']})
-    news_to_return.update({'url' : news['url']})
-    new_content = filter_contents(crop_content(news['contents']))
-    news_to_return.update({'content' : new_content})
+    news_to_return.update({"game": get_game_name_by_id(news["appid"])})
+    news_to_return.update({"title": news["title"]})
+    news_to_return.update({"url": news["url"]})
+    news_to_return.update({"date": datetime.fromtimestamp(news["date"])})
+    # new_content = filter_contents(crop_content(news['contents']))
+    # news_to_return.update({'content' : new_content})
     return create_message(news_to_return)
 
+
 def filter_contents(content):
-    string_to_remove = ''
-    is_append = False  
-    #print(content) 
+    string_to_remove = ""
+    is_append = False
+    # print(content)
     for char in content:
-        if char == '{':
+        if char == "{":
             is_append = True
         if is_append:
-            string_to_remove += (char)
-        if char == '.':
-            is_append = False
-            if string_to_remove+'png ' in content:
-                content = content.replace(string_to_remove+'png ', '')
-            elif string_to_remove+'jpg ' in content:
-                content = content.replace(string_to_remove+'jpg ', '')
-            string_to_remove = ''    
+            string_to_remove += char
     return content
+
 
 def crop_content(content):
     index = 2048
     if len(content) > 2048:
-        while content[index] != ' ':
-            index+=1
-        content = content[:index] + '...'
+        while content[index] != " ":
+            index += 1
+        content = content[:index] + "..."
     return content
 
+
 def create_message(news):
-    game_name = "Game news for " + news['game'] + '\n'
-    news_url = "<a href =\"" + news['url'] + '\">Check full news here</a>' + '\n' 
-    news_title = '<b>' + news['title'] + '</b>' + '\n\n'
-    news_content = news['content'] + '\n'
-    
-    return game_name + news_url + news_title + news_content
-     
+    game_name = "Game news for " + news["game"] + "\n"
+    news_url = '<a href ="' + news["url"] + '">Check full news here</a>' + "\n"
+    news_title = "<b>" + news["title"] + "</b>" + "\n\n"
+    news_date = "<b>Update date: " + str(news["date"]) + "</b>\n\n"
+
+    return game_name + news_url + news_title + news_date
