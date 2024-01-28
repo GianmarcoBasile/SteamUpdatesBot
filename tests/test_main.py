e603a54c1526d4732a639ed34d29fb532afd63a4
GianmarcoBasile <basilemarc2000@gmail.com>
2024-01-28 18:47:05 +0100

    test: change test for ubuntu testing

--- tests/test_main.py
+++ tests/test_main.py
@@ -8,5 +8,4 @@ from main import main
 
 class TestMain(unittest.TestCase):
     def test_main(self):
-        with patch.dict("os.environ", {"API_KEY": "test_api_key"}):
-            self.assertTrue(isinstance(bot, Bot))
+        self.assertTrue(isinstance(bot, Bot))
