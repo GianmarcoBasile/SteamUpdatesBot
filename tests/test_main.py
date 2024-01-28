a5a1dfbb10c56be26d16fbe26fdcbc9819d09fa9
GianmarcoBasile <basilemarc2000@gmail.com>
2024-01-28 18:22:11 +0100

    test: change test for ubuntu testing

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,8 +1,11 @@
 import unittest
+from unittest.mock import patch
 from main import bot_instance as bot
 from bot import Bot
 
 
 class TestMain(unittest.TestCase):
     def test_main(self):
+        with patch("src.main.API_KEY") as mock_api_key:
+            mock_api_key.return_value = "test_api_key"
         self.assertTrue(isinstance(bot, Bot))
