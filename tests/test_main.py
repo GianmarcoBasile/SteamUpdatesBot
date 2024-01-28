31b7ab1c12f4f6a96f01eb4cd9392c71f88baec8
GianmarcoBasile <basilemarc2000@gmail.com>
2024-01-28 18:36:24 +0100

    test: change test for ubuntu testing

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,11 +1,12 @@
 import unittest
-from pytest import MonkeyPatch
+
+from unittest.mock import patch
 from main import bot_instance as bot
 from bot import Bot
+from main import main
 
 
 class TestMain(unittest.TestCase):
     def test_main(self):
-        monkeypatch = MonkeyPatch()
-        monkeypatch.setenv("API_KEY", "test_api_key")
-        self.assertTrue(isinstance(bot, Bot))
+        with patch.dict("os.environ", {"API_KEY": "test_api_key"}):
+            self.assertTrue(isinstance(bot, Bot))
